30bc9212f589853cb457999c7ea090ff011b8d23
Uppalanchi-Venkatesh <uppalanchivenkatesh@gmail.com>
2021-04-30 19:05:30 +0530

    ports changed

--- server.js
+++ server.js
@@ -2,14 +2,15 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-app.use(express.static(__dirname+'/dist/my-resume'));
+app.use(express.static(__dirname+'/dist/MyResume'));
 
 app.get('/*', (req,res)=> {
-    res.sendFile(path.join(__dirname+'/dist/my-resume/index.html'));
+    res.sendFile(path.join(__dirname+'/dist/MyResume/index.html'));
 });
 
-const port = 3000 || process.env.PORT;
+const serve_port = process.env.PORT || 3000;
+const server_host = process.env.YOUR_HOST || '0.0.0.0';
 
-app.listen(port, ()=> {
-    console.log(`server started at port ${port}`);
-})
+app.listen(serve_port,server_host, ()=> {
+    console.log(`server started at port ${serve_port}`);
+});
