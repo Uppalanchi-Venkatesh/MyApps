a8d39bdf707324226ffa8db23121f3a21fc4228d
Uppalanchi-Venkatesh <uppalanchivenkatesh@gmail.com>
2021-05-11 09:43:39 +0530

    Google charts added for verdicts and languages

--- src/assets/JS/Codeforces.js
+++ src/assets/JS/Codeforces.js
@@ -1,8 +1,10 @@
+var verdicts=[],languages=[];
+
 function getInfo() {
     const username = document.getElementById("cf-username").value;
     if(!username) {
         alert("Enter the username");
-        return;
+        return false;
     }
     const URL="https://codeforces.com/api/";
 
@@ -11,77 +13,122 @@ function getInfo() {
             return res.json()
         })
         .then(data => {
-            //console.log(JSON.stringify(data));
+            if(data.status==="FAILED") {
+                alert("Incorrect username");
+                return false;
+            }
             document.getElementById("rating").innerHTML=data.result[0].rating;
             document.getElementById("rank").innerHTML=data.result[0].rank;
             document.getElementById("maxrating").innerHTML=data.result[0].maxRating;
             document.getElementById("maxrank").innerHTML=data.result[0].maxRank;
             document.getElementById("cf-response").style.display="inline";
+
+            fetch(`${URL}user.rating?handle=${username}`)
+                .then(res => {
+                    return res.json();
+                })
+                .then(data1 => {
+                    document.getElementById("cf-ratingchange").innerHTML="";
+                    var table = document.createElement("TABLE");
+                    table.setAttribute("id", "myTable");
+                    table.setAttribute("class", "table");
+                    document.getElementById("cf-ratingchange").appendChild(table);
+                    var header = table.createTHead();
+                    header.setAttribute("id","cf-thead");
+                    document.getElementById("cf-thead").style.fontWeight="bold";
+                    document.getElementById("myTable").style.fontSize="18px";
+                    var row = header.insertRow(0);
+                    var cell1 = row.insertCell(0);
+                    var cell2 = row.insertCell(0);
+                    var cell3 = row.insertCell(0);
+                    var cell4 = row.insertCell(0);
+                    var cell5 = row.insertCell(0);
+                    cell5.innerHTML="ContestID";
+                    cell4.innerHTML="Contest Name";
+                    cell3.innerHTML="Old Rating";
+                    cell2.innerHTML="New Rating";
+                    cell1.innerHTML="Rank";
+                    for(let i=0;i<data1.result.length;i++) {
+                        var tr = document.createElement("TR");
+                        tr.setAttribute("id", "myTr"+(i+1));
+                        document.getElementById("myTable").appendChild(tr);
+                        var td1 = document.createElement("TD");
+                        var td2 = document.createElement("TD");
+                        var td3 = document.createElement("TD");
+                        var td4 = document.createElement("TD");
+                        var td5 = document.createElement("TD");
+                        document.getElementById("myTr"+(i+1)).appendChild(td1);
+                        document.getElementById("myTr"+(i+1)).appendChild(td2);
+                        document.getElementById("myTr"+(i+1)).appendChild(td3);
+                        document.getElementById("myTr"+(i+1)).appendChild(td4);
+                        document.getElementById("myTr"+(i+1)).appendChild(td5);
+                        cell1 = document.createTextNode(data1.result[i].contestId);
+                        cell2 = document.createTextNode(data1.result[i].contestName);
+                        cell3 = document.createTextNode(data1.result[i].oldRating);
+                        cell4 = document.createTextNode(data1.result[i].newRating);
+                        cell5 = document.createTextNode(data1.result[i].rank);
+                        td1.appendChild(cell1);
+                        td2.appendChild(cell2);
+                        td3.appendChild(cell3);
+                        td4.appendChild(cell4);
+                        td5.appendChild(cell5);
+                    }
+                });
+
+                fetch(`${URL}user.status?handle=${username}`)
+                    .then(res=> {
+                        return res.json();
+                    })
+                    .then(data1 => {
+                        var map1=new Map();
+                        var map2=new Map();
+                        for(let i=0;i<data1.result.length;i++) {
+                            let key1=data1.result[i].verdict;
+                            let key2=data1.result[i].programmingLanguage;
+                            if(map1.has(key1))
+                                map1.set(key1,map1.get(key1)+1);
+                            else
+                                map1.set(key1,1);
+                            if(map2.has(key2))
+                                map2.set(key2,map2.get(key2)+1);
+                            else
+                                map2.set(key2,1);
+                        }
+                        map1.forEach((key,value) => verdicts.push([value,key]));
+                        google.charts.load('current', {'packages':['corechart']});
+                        google.charts.setOnLoadCallback(draw1);
+
+                        map2.forEach((key,value) => languages.push([value,key]));
+                        google.charts.load('current', {'packages':['corechart']});
+                        google.charts.setOnLoadCallback(draw2);
+                    });
         })
         .catch(err => {
-            //alert("Incorrect username or some error occured");
-            console.log('User not found');
+            alert("Some error occured");
             return;
         });
+}
 
-    fetch(`${URL}user.rating?handle=${username}`)
-        .then(res => {
-                return res.json();
-        })
-        .then(data => {
-            //console.log(JSON.stringify(data));
-            //console.log(data.result[0]);
-            document.getElementById("cf-ratingchange").innerHTML="";
-            var table = document.createElement("TABLE");
-            table.setAttribute("id", "myTable");
-            table.setAttribute("class", "table");
-            document.getElementById("cf-ratingchange").appendChild(table);
-            var header = table.createTHead();
-            header.setAttribute("id","cf-thead");
-            document.getElementById("cf-thead").style.fontWeight="bold";
-            document.getElementById("myTable").style.fontSize="18px";
-            var row = header.insertRow(0);
-            var cell1 = row.insertCell(0);
-            var cell2 = row.insertCell(0);
-            var cell3 = row.insertCell(0);
-            var cell4 = row.insertCell(0);
-            var cell5 = row.insertCell(0);
-            cell5.innerHTML="ContestID";
-            cell4.innerHTML="Contest Name";
-            cell3.innerHTML="Old Rating";
-            cell2.innerHTML="New Rating";
-            cell1.innerHTML="Rank";
-            for(let i=0;i<data.result.length;i++) {
-                var tr = document.createElement("TR");
-                tr.setAttribute("id", "myTr"+(i+1));
-                document.getElementById("myTable").appendChild(tr);
-                var td1 = document.createElement("TD");
-                var td2 = document.createElement("TD");
-                var td3 = document.createElement("TD");
-                var td4 = document.createElement("TD");
-                var td5 = document.createElement("TD");
-                document.getElementById("myTr"+(i+1)).appendChild(td1);
-                document.getElementById("myTr"+(i+1)).appendChild(td2);
-                document.getElementById("myTr"+(i+1)).appendChild(td3);
-                document.getElementById("myTr"+(i+1)).appendChild(td4);
-                document.getElementById("myTr"+(i+1)).appendChild(td5);
-                cell1 = document.createTextNode(data.result[i].contestId);
-                cell2 = document.createTextNode(data.result[i].contestName);
-                cell3 = document.createTextNode(data.result[i].oldRating);
-                cell4 = document.createTextNode(data.result[i].newRating);
-                cell5 = document.createTextNode(data.result[i].rank);
-                td1.appendChild(cell1);
-                td2.appendChild(cell2);
-                td3.appendChild(cell3);
-                td4.appendChild(cell4);
-                td5.appendChild(cell5);
-            }
-        })
-        .catch(err => {
-            console.log("Error Occurred");
-            alert("Incorrect username or some error occured");
-            return;
-        })
+function draw1() {
+    var data = new google.visualization.DataTable();
+    data.addColumn('string', 'solvedcount');
+    data.addColumn('number', 'Slices');
+    data.addRows(verdicts);
+          
+    var options = {'title':'Your verdicts of submitted problems','width':500,'height':400};
+    var chart = new google.visualization.PieChart(document.getElementById("cf-verdictchart"));
+    chart.draw(data, options);
+}
+
+function draw2() {
+    var data = new google.visualization.DataTable();
+    data.addColumn('string', 'solvedcount');
+    data.addColumn('number', 'Slices');
+    data.addRows(languages);
+          
+    var options = {'title':'Languages used','width':500,'height':400};
+    var chart = new google.visualization.PieChart(document.getElementById("cf-languagechart"));
+    chart.draw(data, options);
 }
 
 
--- src/assets/JS/GoogleChart.js
+++ src/assets/JS/GoogleChart.js
@@ -1,21 +1,20 @@
 google.charts.load('current', {'packages':['corechart']});
-
 google.charts.setOnLoadCallback(drawChart);
 
-function drawChart(){
-    var data = new google.visualization.DataTable();
-        data.addColumn('string', 'solvedcount');
-        data.addColumn('number', 'Slices');
-        data.addRows([
-          ['Leetcode', 30],
-          ['Codechef', 46],
-          ['Codeforces', 144],
-          ['Vjudge', 112],
-          ['Mentorpick', 164]
-        ]);
+function drawChart() {
+  var data = new google.visualization.DataTable();
+  data.addColumn('string', 'solvedcount');
+  data.addColumn('number', 'Slices');
+  data.addRows([
+      ['Leetcode', 30],
+      ['Codechef', 46],
+      ['Codeforces', 144],
+      ['Vjudge', 112],
+      ['Mentorpick', 164]
+  ]);
         
-    var options = {'title':'Problems solved in different Coding Platforms','width':500,'height':400};
+  var options = {'title':'Problems solved in different Coding Platforms','width':500,'height':400};
 
-    var chart = new google.visualization.PieChart(document.getElementById("chart"));
-    chart.draw(data, options);
+  var chart = new google.visualization.PieChart(document.getElementById("chart"));
+  chart.draw(data, options);
 }
