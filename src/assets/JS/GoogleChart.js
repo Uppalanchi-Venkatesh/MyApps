8a16c46f7ac8b8cf7dc21e24c0544871252ac687
Uppalanchi-Venkatesh <uppalanchivenkatesh@gmail.com>
2021-05-13 11:35:32 +0530

    Error handled

--- src/assets/JS/GoogleChart.js
+++ src/assets/JS/GoogleChart.js
@@ -1,20 +1,23 @@
-google.charts.load('current', {'packages':['corechart']});
-google.charts.setOnLoadCallback(drawChart);
+window.onload = () => {
 
-function drawChart() {
-  var data = new google.visualization.DataTable();
-  data.addColumn('string', 'solvedcount');
-  data.addColumn('number', 'Slices');
-  data.addRows([
-      ['Leetcode', 30],
-      ['Codechef', 46],
-      ['Codeforces', 144],
-      ['Vjudge', 112],
-      ['Mentorpick', 164]
-  ]);
-        
-  var options = {'title':'Problems solved in different Coding Platforms','width':500,'height':400};
+  google.charts.load('current', {'packages':['corechart']});
+  google.charts.setOnLoadCallback(drawChart);
 
-  var chart = new google.visualization.PieChart(document.getElementById("chart"));
-  chart.draw(data, options);
+  function drawChart() {
+    var data = new google.visualization.DataTable();
+    data.addColumn('string', 'solvedcount');
+    data.addColumn('number', 'Slices');
+    data.addRows([
+        ['Leetcode', 30],
+        ['Codechef', 46],
+        ['Codeforces', 144],
+        ['Vjudge', 112],
+        ['Mentorpick', 164]
+    ]);
+          
+    var options = {'title':'Problems solved in different Coding Platforms','width':500,'height':400};
+
+    var chart = new google.visualization.PieChart(document.getElementById("chart"));
+    chart.draw(data, options);
+  }
 }
